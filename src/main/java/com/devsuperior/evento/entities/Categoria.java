0229823aa96830b2c53071471ac6d74c53afa49f
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-14 17:30:25 -0400

    add de atributo atividades

--- src/main/java/com/devsuperior/evento/entities/Categoria.java
+++ src/main/java/com/devsuperior/evento/entities/Categoria.java
@@ -1,9 +1,13 @@
 package com.devsuperior.evento.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,8 +16,11 @@ public class Categoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY	)
-	private Long id;
+	private Integer id;
 	
 	private String descricao;
+	
+	@OneToMany(mappedBy = "id.categoria")
+	private Set<Atividade> atividades = new HashSet<>();
 
 }
