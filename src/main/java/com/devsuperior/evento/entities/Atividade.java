bbd68517413aef9150ccf02523e0092589d35cc6
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-15 22:09:27 -0400

    Mudança no relacionamento entre entidades usando @OneToMany,@ManyToMany

--- src/main/java/com/devsuperior/evento/entities/Atividade.java
+++ src/main/java/com/devsuperior/evento/entities/Atividade.java
@@ -1,38 +1,102 @@
 package com.devsuperior.evento.entities;
 
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_atividade")
 public class Atividade {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY	)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String nome;
 	private String descricao;
 	private Double preco;
-	
-	@OneToOne(mappedBy = "id.atividade", cascade = CascadeType.ALL)
+
+	@ManyToOne
+	@JoinColumn(name = "categotia_id")
 	private Categoria categoria;
-	
-	@OneToMany(mappedBy = "id.atividade")
-	private Set<Participante> participantes = new HashSet<>();
-	
-	@OneToMany(mappedBy = "id.atividade")
-	private Set<Bloco> blocos = new HashSet<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante", 
+	joinColumns = @JoinColumn(name = "atividade_id"), 
+	inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private List<Participante> participantes;
+
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos;
+
+	public Atividade() {
+
+	}
+
+	public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	public List<Bloco> getBlocos() {
+		return blocos;
+	}
 
 }
