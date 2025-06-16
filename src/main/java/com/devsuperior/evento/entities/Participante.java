0dbaf26b1443f70e6c7c2be96d2fcd971116ec25
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-15 22:10:22 -0400

    Mudança no relacionamento entre entidades usando @OneToMany,@ManyToMany

--- src/main/java/com/devsuperior/evento/entities/Participante.java
+++ src/main/java/com/devsuperior/evento/entities/Participante.java
@@ -1,29 +1,64 @@
 package com.devsuperior.evento.entities;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_participante")
 public class Participante {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY	)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String nome;
-	
+
 	private String email;
-	
-	@OneToMany(mappedBy = "id.participante")
-	private Set<Atividade> atividades = new HashSet<>();
+
+	@ManyToMany(mappedBy = "participantes")
+	private List<Atividade> atividades;
+
+	public Participante() {
+
+	}
+
+	public Participante(Integer id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	
 
 }
