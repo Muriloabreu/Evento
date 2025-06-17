581f80e2534444afa4914d8659040fc3c46a5aa8
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-16 21:03:11 -0400

    add de hashcode e toString

--- src/main/java/com/devsuperior/evento/entities/Bloco.java
+++ src/main/java/com/devsuperior/evento/entities/Bloco.java
@@ -1,6 +1,7 @@
 package com.devsuperior.evento.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -62,5 +63,28 @@ public class Bloco {
 		this.fim = fim;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bloco other = (Bloco) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Bloco [id=" + id + ", inicio=" + inicio + ", fim=" + fim + ", atividade=" + atividade + "]";
+	}
+
+	
 	
 }
